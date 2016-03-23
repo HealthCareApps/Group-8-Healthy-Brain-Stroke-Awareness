a5517441f1ea67048de7e2bce65fb39f2d4738ae
pprutzer7 <pprutzer2014@fau.edu>
2016-03-23 12:54:52 -0400

    ini

---
+++ app/src/main/java/com/example/strokeapp2/Ipsum.java
@@ -0,0 +1,19 @@
+package com.example.strokeapp2;
+
+
+public class Ipsum {
+
+    static String[] Headlines = {
+            "Article One",
+            "Article Two"
+    };
+
+    static String[] Articles = {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F"
+    };
+}
